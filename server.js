9a95dbd901779a41149821eb342bc138d1f0488c
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-07 18:48:23 +0800

    testing heroku deployment with mongodb

--- server.js
+++ server.js
@@ -11,8 +11,26 @@ const testController = require("./controllers/testController");
 // const PASSWORD = anFbOQsQYv1M45Uw;
 const app = express();
 const port = process.env.PORT ?? 3001;
+
 const MONGODB_URI =
   process.env.MONGODB_URI ?? `mongodb://localhost:27017/trivia`;
+const db = mongoose.connection;
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true
+  },
+  () => {
+    console.log("mongo cloud connection established");
+  }
+);
+// ERROR / SUCCESS
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 //! change database host to atlas mongodb database @Qai
 mongoose.connect(MONGODB_URI);
 mongoose.connection.once("open", () => {
